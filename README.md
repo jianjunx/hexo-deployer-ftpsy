bf85db9fb3d82b6037a0ab7ba6e9cc732c1a33ea
jianjun.xie <jaykenxie@outlook.com>
2020-09-11 17:28:56 +0800

    修改报错

---
+++ README.md
@@ -0,0 +1,95 @@
+hexo-deployer-ftpsy
+===================
+
+FTP deployer plugin for http://hexo.io/
+
+## Usage
+
+### Install
+
+```
+npm install hexo-deployer-ftpsy --save
+```
+
+### Enable
+
+Set `deploy.type` to `ftp` in `_config.yml`.
+
+``` yaml
+deploy:
+  type: ftp
+```
+
+### Configure
+
+Add `connection` and `root` to `deploy` in `_config.yml`.
+
+```
+deploy:
+  type: ftp
+  connection:
+    host: <ftp host>
+    port: <ftp port>
+    user: <ftp user>
+    password: <ftp password>
+    ...
+  root: <path/to/your/blog/on/the/server>
+```
+
+Connection properties:
+
+* host - _string_ - The hostname or IP address of the FTP server. **Default:** 'localhost'
+
+* port - _integer_ - The port of the FTP server. **Default:** 21
+
+* secure - _mixed_ - Set to true for both control and data connection encryption, 'control' for control connection encryption only, or 'implicit' for implicitly encrypted control connection (this mode is deprecated in modern times, but usually uses port 990) **Default:** false
+
+* secureOptions - _object_ - Additional options to be passed to `tls.connect()`. **Default:** (none)
+  * Additionally you can add pfxPath, keyPath, caPath and certPath to specify the path of the file that contains data. This parameters are overrides existing ones.
+
+* user - _string_ - Username for authentication. **Default:** 'anonymous'
+
+* password - _string_ - Password for authentication. **Default:** 'anonymous@'
+
+* connTimeout - _integer_ - How long (in milliseconds) to wait for the control connection to be established. **Default:** 10000
+
+* pasvTimeout - _integer_ - How long (in milliseconds) to wait for a PASV data connection to be established. **Default:** 10000
+
+* keepalive - _integer_ - How often (in milliseconds) to send a 'dummy' (NOOP) command to keep the connection alive. **Default:** 10000
+
+### Usage
+
+Type deploy command in console.
+```
+hexo deploy [host=<ftp host>] [port=<ftp port>] [user=<ftp user>] [password=<ftp password>]
+```
+Command line parameters overrides the default configuration.
+
+### Disable
+
+Remove `ftp` value from `deploy.type` in `_config.yml`.
+
+``` yaml
+deploy:
+  type:
+```
+
+### Update
+
+Execute the following command.
+
+```
+npm update
+```
+
+### Uninstall
+
+Execute the following command. Don't forget to disable the plugin before uninstalling.
+
+```
+npm uninstall hexo-deployer-ftpsy
+```
+
+----------
+
+[Hexo]: http://hexo.io
---
+++ index.js
@@ -0,0 +1,235 @@
+var fs = require('fs'),
+  Ftp = require('ftp');
+
+var client = new Ftp();
+var count;
+
+var public_dir = hexo.base_dir + (hexo.config.public_dir || '/public');
+if (public_dir.slice(-1) === '/') {
+  public_dir = public_dir.substring(0, public_dir.length - 1);
+}
+
+function listFiles(path, callback) {
+
+  var results = {
+    'path': path,
+    'files': [],
+    'directories': {}
+  };
+
+  fs.readdir(path, function (error, list) {
+
+    if (error) {
+      return callback(error);
+    }
+
+    var pending = list.length;
+
+    if (!pending) {
+      return callback(null, results);
+    }
+
+    list.forEach(function (file) {
+
+      var fullPath = path + '/' + file;
+
+      fs.stat(fullPath, function (error, stat) {
+
+        if (stat && stat.isDirectory()) {
+
+          listFiles(fullPath, function (error, res) {
+
+            results.directories[file] = res;
+
+            if (!--pending) {
+              callback(null, results);
+            }
+
+          });
+
+        }
+        else {
+
+          results.files.push(file);
+
+          if (!--pending) {
+            callback(null, results);
+          }
+
+        }
+
+      });
+
+    });
+
+  });
+
+}
+
+function uploadDirectory (structure, callback) {
+
+  var f = 0,
+    d = 0,
+    dirNames = Object.keys(structure.directories);
+
+  var nextDir = function () {
+
+    var dirName = dirNames[d++];
+
+    if (!dirName) {
+      return callback();
+    }
+
+    client.mkdir(dirName, false, function (error) {
+
+      client.cwd(dirName, function (error) {
+
+        if (error) {
+          return callback(error);
+        }
+
+        uploadDirectory(structure.directories[dirName], function () {
+
+          client.cdup(function (error) {
+
+            if (error) {
+              return callback(error);
+            }
+
+            nextDir();
+
+          });
+
+        });
+
+      });
+
+    });
+
+  };
+
+  var nextFile = function () {
+
+    var file = structure.files[f++];
+
+    if (!file) {
+      return nextDir();
+    }
+
+    client.put(structure.path + '/' + file, file, function (error) {
+
+      if (error) {
+        return callback(error);
+      }
+
+      hexo.log.log('upload', 'Uploaded: %s', structure.path + '/' + file);
+      count++;
+
+      nextFile();
+
+    });
+
+  };
+
+  nextFile();
+
+}
+
+function getConnOpts(args) {
+
+  var connOpts = {},
+
+    setConnProp = function(prop) {
+      if(typeof args.connection[prop] !== 'undefined') {
+        connOpts[prop] = args.connection[prop];
+      }
+    },
+
+    readCert = function(prop) {
+      if(connOpts.secureOptions[prop + 'Path'] && fs.existsSync(connOpts.secureOptions[prop + 'Path'])) {
+        connOpts.secureOptions[prop] = fs.readFileSync(connOpts.secureOptions[prop + 'Path']);
+      }
+    };
+
+  if (args.connection) {
+
+    ['host', 'port', 'secure', 'secureOptions', 'user', 'password', 'connTimeout', 'pasvTimeout', 'keepalive'].forEach(setConnProp);
+
+    if(connOpts.secureOptions) {
+
+      ['pfx', 'key', 'ca', 'cert'].forEach(readCert);
+
+    }
+
+  }
+
+  for (var key in args._) {
+
+    var match = args._[key].match(/^([^=]*)=(.*)$/);
+
+    if (match && match.length === 3) {
+      connOpts[match[1]] = match[2];
+    }
+
+  }
+
+  return connOpts;
+
+}
+
+hexo.extend.deployer.register('ftp', function (args, callback) {
+
+  // hexo.log.setAlias('upload', 'info'); // not such setAlias function for hexo-log.
+
+  var start = Date.now();
+  count = 0;
+
+  var root = (args.root || '');
+
+  var finish = function () {
+
+    client.end();
+    return callback.apply(this, arguments);
+
+  }
+
+  listFiles(public_dir, function (error, structure) {
+
+    if (error) {
+      return callback(error);
+    }
+
+    client.on('ready', function () {
+
+      client.mkdir(root, function(error) {
+
+        client.cwd(root, function (error, currentDir) {
+
+          if (error) {
+            return finish(error);
+          }
+
+          uploadDirectory(structure, function (error) {
+
+            if (error) {
+              return finish(error);
+            }
+
+            var elapsed = (Date.now() - start) / 1000;
+
+            hexo.log.log('upload', '%d files uploaded in %ss', count, elapsed.toFixed(3));
+            return finish();
+
+          });
+
+        });
+
+      });
+
+    });
+
+    client.connect(getConnOpts(args));
+
+  });
+
+})
